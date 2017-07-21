35b1d2746c55b5b375cfdc6d3139e690f3ceb8a7
victorsalaun <victor.salaun@gmail.com>
2017-07-21 23:39:34 +0200

    add CloudFormation file for lambda

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 ## Prerequisites
 
-- aws-cli
+- aws-cli (latest)
+- 1 Amazon Cognito users pool
 
 ## Deploy
 
@@ -10,7 +11,8 @@
 
 Deploy :
 
-    aws cloudformation deploy --template-file cloudformation\skillbrowser-master.yml --stack-name skillbrowser-master --region eu-central-1
+    cd cloudformation
+    aws cloudformation deploy --template-file skillbrowser-master.yml --stack-name skillbrowser-master --region eu-central-1
 
 Outputs :
 
@@ -20,8 +22,36 @@ Outputs :
 
 Deploy :
 
-    aws cloudformation deploy --template-file cloudformation\skillbrowser-dynamodb.yml --stack-name skillbrowser-dynamodb --region eu-central-1
+    cd cloudformation
+    aws cloudformation deploy --template-file skillbrowser-dynamodb.yml --stack-name skillbrowser-dynamodb --region eu-central-1
 
 Outputs :
 
     aws cloudformation describe-stacks --stack-name skillbrowser-dynamodb --region eu-central-1
+
+### Lambda
+
+Zip :
+
+    cd lambda
+    
+    // For Windows use PowerShell
+    zip.ps1
+    
+    // For Unix
+    zip.sh
+
+Deploy :
+
+Fill skillbrowser-lambda.parameters.json with Master outputs and DynamoDB outputs
+
+    cd lambda
+        
+    // For Windows use PowerShell
+    deploy.ps1
+        
+    // For Unix
+    deploy.sh
+    
+    cd cloudformation
+    aws cloudformation create-stack --template-body file://skillbrowser-lambda.yml --parameters file://skillbrowser-lambda.parameters.json --stack-name skillbrowser-lambda --capabilities CAPABILITY_IAM --region eu-central-1
---
+++ lambda/deploy.ps1
@@ -0,0 +1,22 @@
+$lambda_s3 = "skillbrowser-lambda-s3"
+$region = "eu-central-1"
+
+aws s3 cp createClientFunction.zip      s3://$lambda_s3/createClientFunction.zip        --region $region
+aws s3 cp deleteClientFunction.zip      s3://$lambda_s3/deleteClientFunction.zip        --region $region
+aws s3 cp readClientFunction.zip        s3://$lambda_s3/readClientFunction.zip          --region $region
+aws s3 cp updateClientFunction.zip      s3://$lambda_s3/updateClientFunction.zip        --region $region
+
+aws s3 cp createEmployeeFunction.zip    s3://$lambda_s3/createEmployeeFunction.zip      --region $region
+aws s3 cp deleteEmployeeFunction.zip    s3://$lambda_s3/deleteEmployeeFunction.zip      --region $region
+aws s3 cp readEmployeeFunction.zip      s3://$lambda_s3/readEmployeeFunction.zip        --region $region
+aws s3 cp updateEmployeeFunction.zip    s3://$lambda_s3/updateEmployeeFunction.zip      --region $region
+
+aws s3 cp createMissionFunction.zip     s3://$lambda_s3/createMissionFunction.zip       --region $region
+aws s3 cp deleteMissionFunction.zip     s3://$lambda_s3/deleteMissionFunction.zip       --region $region
+aws s3 cp readMissionFunction.zip       s3://$lambda_s3/readMissionFunction.zip         --region $region
+aws s3 cp updateMissionFunction.zip     s3://$lambda_s3/updateMissionFunction.zip       --region $region
+
+aws s3 cp createSkillFunction.zip       s3://$lambda_s3/createSkillFunction.zip         --region $region
+aws s3 cp deleteSkillFunction.zip       s3://$lambda_s3/deleteSkillFunction.zip         --region $region
+aws s3 cp readSkillFunction.zip         s3://$lambda_s3/readSkillFunction.zip           --region $region
+aws s3 cp updateSkillFunction.zip       s3://$lambda_s3/updateSkillFunction.zip         --region $region
